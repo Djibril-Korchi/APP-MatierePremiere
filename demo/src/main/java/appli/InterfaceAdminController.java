0a54d8297723279de4c2f1fc19737dcb9dab4793
Djibril KORCHI <KORCHI_Dj@lprs.local>
2024-03-01 10:44:00 +0100

    feat:Admin, liaison page

--- demo/src/main/java/appli/InterfaceAdminController.java
+++ demo/src/main/java/appli/InterfaceAdminController.java
@@ -4,15 +4,22 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class InterfaceController {
+public class InterfaceAdminController {
 
     @FXML
     private Button bon;
+    @FXML
+    private Button Admin;
 
     @FXML
     void onClick(ActionEvent event) {
 
         HelloApplication helloApplication= new HelloApplication();
         helloApplication.sceneConnexion("bonDebit");
+    }
+    @FXML
+    void onClickAdmin(ActionEvent event) {
+        HelloApplication helloApplication= new HelloApplication();
+        helloApplication.sceneConnexion("Admin");
     }
 }
