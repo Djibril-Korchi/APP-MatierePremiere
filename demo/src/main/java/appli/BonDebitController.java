4370ec35eebbc9b1f8dc61ba1807cdf36e7f1410
Rayane Menkar <rayane.menkar@gmail.com>
2024-03-08 08:57:15 +0100

    Ajout d'un bouton retour dans bon de debit

--- demo/src/main/java/appli/BonDebitController.java
+++ demo/src/main/java/appli/BonDebitController.java
@@ -1,4 +1,18 @@
 package appli;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class BonDebitController {
+
+    @FXML
+    private Button retour;
+
+    @FXML
+    void OnClickRetour(ActionEvent event) {
+        HelloApplication helloApplication = new HelloApplication();
+        helloApplication.sceneConnexion("InterfaceAdmin.fxml");
+    }
+
 }
