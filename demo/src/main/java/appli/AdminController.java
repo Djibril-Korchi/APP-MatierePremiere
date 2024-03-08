c8ef7a169da60c2e1279fded12227d102ed113bf
Djibril KORCHI <KORCHI_Dj@lprs.local>
2024-03-08 08:13:37 +0100

    interface

--- demo/src/main/java/appli/AdminController.java
+++ demo/src/main/java/appli/AdminController.java
@@ -1,5 +1,41 @@
 package appli;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class AdminController {
+
+    @FXML
+    private Button ajouter;
+
+    @FXML
+    private Button modifier;
+
+    @FXML
+    private Button retour;
+
+    @FXML
+    private Button supprimer;
+
+    @FXML
+    void onClickAjouter(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickModifier(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickRetour(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSuprimer(ActionEvent event) {
+
+    }
 
 }
--- demo/src/main/java/appli/BonDebitController.java
+++ demo/src/main/java/appli/BonDebitController.java
@@ -1,4 +1,25 @@
 package appli;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class BonDebitController {
+
+    @FXML
+    private Button retour;
+
+    @FXML
+    private Button valider;
+
+    @FXML
+    void onActionRetour(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickValider(ActionEvent event) {
+
+    }
+
 }
